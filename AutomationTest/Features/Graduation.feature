f74e00d1aefbe73177a7741c27cb48d0d850b601
Renato Gregorio <srgregorio1@gmail.com>
2025-01-17 21:28:20 -0300

    feat: adicionando a feature e configurando inicialmente os steps

--- AutomationTest/Features/Graduation.feature
+++ AutomationTest/Features/Graduation.feature
@@ -1,9 +1,9 @@
-Feature: Inscrição de Usuário para curso de Graduação
+Feature: Inscricao de Usuario para curso de Graduacao
 
-Scenario: Inscrição graduação ao portal no Chrome
+  Scenario: Inscricao graduação ao portal no Chrome
     Given que estou na pagina de login Grupo A
     When eu selecionar o nivel de ensino
-    When selecionar um curso
+	When selecionar um curso
     When preencher o formulario de cadastro
     When fazer login com meu usuario cadastrado
     Then devo visualizar a pagina interna do candidato
---
+++ AutomationTest/Steps/GraduationStep.cs
@@ -0,0 +1,45 @@
+using Reqnroll;
+
+namespace AutomationTest.Steps
+{
+    [Binding]
+    public class GraduationStep
+    {
+        [Given("que estou na pagina de login Grupo A")]
+        public void GivenEstouNaPaginaDeLoginGrupoA()
+        {
+
+        }
+
+        [When("eu selecionar o nivel de ensino")]
+        public void WhenEuSelecionarONivelDeEnsino()
+        {
+
+        }
+
+        [When("selecionar um curso")]
+        public void WhenSelecionarUmCurso()
+        {
+
+        }
+
+        [When("preencher o formulario de cadastro")]
+        public void WhenPreencherOFormularioDeCadastro()
+        {
+
+
+        }
+
+        [When("fazer login com meu usuario cadastrado")]
+        public void WhenFazerLoginComMeuUsuarioCadastrado()
+        {
+
+        }
+
+        [Then("devo visualizar a pagina interna do candidato")]
+        public void ThenDevoVisualizarAPaginaInternaDoCandidato()
+        {
+            
+        }
+    }
+}
